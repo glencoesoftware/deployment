80f86bd9c576585cd8e8c0f8bc3511e3aaf6becb
Chris MacLeod <ckm@glencoesoftware.com>
2013-08-16 14:15:29 -0400

    make deploy.sh a bit more flexible based on new schematic

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,11 @@ VERSION_DIR=$( zipinfo -1 OMERO.py-*-b${BUILD_NUMBER}.zip |head -1 )
 unzip -o OMERO.py-*-b$BUILD_NUMBER.zip
 
 # copy configs into new build dir
-cp -fvr config/* $VERSION_DIR/
+# cp -fvr config/* $VERSION_DIR/
+
+# symlink etc dir above the deployment directory (2 above the version directory)
+rm -rf $VERSION_DIR/etc
+ln -T -s -vf ../../etc $VERSION_DIR/etc
 
 # switch between running omero webs
 ./toggle-omeroweb.sh current $VERSION_DIR
